f428c6fae4537ee788ffb64c3fd481c0c8263cea
Kevin Smekens <kjsmekens@gmail.com>
2018-07-28 17:39:54 +0800

    yelp restaurants and reviews working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:delete, :update] do
+    resources :reviews, except: [:delete, :update]
+  end
+  root to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+ {
+   name:         'Dishoom',
+   address:      '7 Boundary St, London E2 7JE',
+   phone_number:  '045-5689392',
+   category:       'japanese'
+ },
+ {
+   name:         'Pizza East',
+   address:      '56A Shoreditch High St, London E1 6PQ',
+   phone_number:  '045-5689392',
+   category:       'italian'
+ },
+{
+   name:         'Shady Shack',
+   address:      'JL BatuBolong, Canggu 96 B',
+   phone_number:  '063-89892929',
+   category:       'french'
+ },
+{
+   name:         'Canteen',
+   address:      'JL Eco Beach, Canngu 123 Q',
+   phone_number:  '063-58982829',
+   category:       'belgian'
+ },
+
+{
+   name:         'Laroy',
+   address:      'JL Eco Beach, Canngu 18 Z',
+   phone_number:  '063-58988929',
+   category:       'chinese'
+ }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
